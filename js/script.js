6101c062dca9254ae4cec843e22e16f6df2fe895
virtu-alex <alessio.cordari@gmail.com>
2022-06-30 14:07:34 +0200

    MILESTONE 2

--- js/script.js
+++ js/script.js
@@ -7,12 +7,10 @@ Procediamo milestone per milestone senza invertire l'ordine.
 Cerchiamo di fare almeno un commit per ogni milestone!
 
 
-# MILESTONE 1
+////# MILESTONE 1
 Prepariamo l'HTML ed il CSS per ottenere il risultato grafico che vediamo nell'immagine allegata.
 
 
-#MILESTONE 2
-Rimuoviamo le celle che abbiamo inserito nell'HTML in modo da generarle tramite JS. Al click del bottone play, vengono generate 100 celle in 10 righe da 10 celle ciascuna.
 
 
 #MILESTONE 3
@@ -29,3 +27,36 @@ Aggiungere una select accanto al bottone di generazione, che fornisca una scelta
 - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
 - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;*/
+
+
+/*#MILESTONE 2
+Rimuoviamo le celle che abbiamo inserito nell'HTML in modo da generarle tramite JS. Al click del bottone play, vengono generate 100 celle in 10 righe da 10 celle ciascuna.*/
+
+//FUNZIONE CREAZIONE CELLA
+function createCell() {
+    const cell = document.createElement('div');
+    cell.className = 'cell'
+    return cell;
+}
+//RECUPERO ELEMENTO DAL DOM
+const grid = document.getElementById('grid');
+const button = document.getElementById('button')
+
+//IMPOSTAZIONI INIZIALI X DETERMINARE IL NUMERO DI RIGHE E COLONNE
+
+const rows = 10;
+const cells = 10;
+const totalCells = rows * cells;
+
+
+// MOSTRO IN PAGINE LA GRIGLIA TRAMITE UN EVENT LISTNER
+button.addEventListener('click', () => {
+
+    for (let i = 1; i <= totalCells; i++) {
+        const cell = createCell();
+        grid.appendChild(cell);
+    }
+    
+})
+
+
